c66f4e3c38c81f1965e3743d52a8fa266e302b11
roycabouly <53654303+htr751@users.noreply.github.com>
2020-01-04 23:30:25 +0200

    Update MyPrintf.h

--- MyPrintf.h
+++ MyPrintf.h
@@ -19,11 +19,11 @@ constexpr int CheckPrintf(std::string_view format, T firstArgument, Args&&... ot
 		throw "too many arguments";
 
 	if (format.at(index + 1) == 'd') {
-		if (!std::is_same_v<int, decltype(firstArgument)>)
+		if constexpr(!std::is_same_v<int, decltype(firstArgument)>)
 			throw  "Error: invalid argument type";
 	}
 	else if (format.at(index + 1) == 's') {
-		if (!std::is_same_v<std::string_view, decltype(firstArgument)>)
+		if constexpr(!std::is_same_v<std::string_view, decltype(firstArgument)>)
 			throw "Error: invalid argument type";
 	}
 
